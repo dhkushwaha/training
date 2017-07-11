662ac5413dfed171f1e8f8bff7fa5a5f9bade353
dhkushwaha <dhananjay@ceegees.in>
2017-07-11 19:11:33 +0530

    Up to distributed card is sorted

--- dhananjay/card.js
+++ dhananjay/card.js
@@ -104,12 +104,12 @@ function addCard(deck){
         }
     
     var noOfP = playerName.length;
-    console.log("No of Players on the table is " + noOfP);
-    console.log(this.Players);
+    console.log("No of Players on the table is " + noOfP + " And players are");
+    console.log(playerName);
     
     
     if(noOfP <= 6){
-      console.log("lets distribute the card ");
+      console.log("lets distribute the card among the above players");
       //console.log(deck.length);
       
       function cardDistribution(){
@@ -133,7 +133,8 @@ function addCard(deck){
           
           return cardTaken;
         }
-        console.log(Players);
+          
+        console.log(this.Players);
         console.log("lets calculate the score of the each players");
           
         for (var j = 0; j < noOfP; j++){
@@ -202,103 +203,48 @@ function addCard(deck){
                    
                 }
                /*console.log(Players[j].hand[0].value);  */                 
+              
+                   
+                  
+                }
+            
+               for (var i = 0; i < cardToDist; i++) {
                var scoresOfPlayers = [Players[j].hand[0].value, Players[j].hand[1].value, Players[j].hand[2].value,
                                       Players[j].hand[3].value, Players[j].hand[4].value, Players[j].hand[5].value, 
                                       Players[j].hand[6].value, Players[j].hand[7].value, Players[j].hand[8].value, 
-                                      Players[j].hand[9].value,].reduce(add, 0);
-
+                                      Players[j].hand[9].value].reduce(add, 0);
+                   
+      
                function add(a, b) {
                  
                return a + b;
                    
                  }
-
+               }
+               console.log(scoresOfPlayers);
+               function sum(scoresOfPlayers) {
+                   
+               var scoresOfPlayers = [Players[j].hand[0].value, Players[j].hand[1].value, Players[j].hand[2].value,
+                                      Players[j].hand[3].value, Players[j].hand[4].value, Players[j].hand[5].value, 
+                                      Players[j].hand[6].value, Players[j].hand[7].value, Players[j].hand[8].value, 
+                                      Players[j].hand[9].value];
+            
+               scoresOfPlayers.sort(function(a, b){return a-b});
+            
                console.log(scoresOfPlayers);
                
+               var s = 0;
+                   for(var i = 0; i < scoresOfPlayers.length; i++) {
+                       s = s + scoresOfPlayers[i];
+                   }
+                   console.log(s)
               
-                       
-              
-                }
-
+                  
+              }
+              sum(scoresOfPlayers);
              }
              
-            
-       /* function getCardValue() {
-        var value = 0;
-        var totalValues=0;
-        for (var j = 0; j < noOfP; j++) {
-            for (var i = 0; i < cardToDist; i++) {
-
-
-                switch (Players[j].hand[i].value) {
-                        
-                    case "A":
-                        totalValues = value + 14;
-
-                        break;
-                        
-                    case 2:
-                      totalValues = value + 2;
- 
-                        break;
-                    case 3:
-                        totalValues = value + 3;
-
-                        break;
-                    case 4:
-                        totalValues = value + 4;
-
-                        break;
-                    case 5:
-                        totalValues = value + 5;
-
-                        break;
-                    case 6:
-                        totalValues = value + 6;
-
-                        break;
-                    case 7:
-                        totalValues = value + 7;
-
-                        break;
-                    case 8:
-                        totalValues = value + 8;
-
-                        break;
-                    case 9:
-                        totalValues = value + 9;
-
-                        break;
-                    case 10:
-                        totalValues = value + 10;
-
-                        break;
-                    case "J":
-                        totalValues = value + 11;
-
-                        break;
-                    case "Q":
-                        totalValues = value + 12;
-
-                        break;
-                    case "K":
-                        totalValues = value + 13;
-
-                        break;
-                    
-
-                    default:
-                        totalValues = 0;
-                        break;
-                }
-
-                console.log(totalValues);
-            }
-        }
-    }
-    getCardValue(); */
-
-       
+             
       }
       cardDistribution();
       
